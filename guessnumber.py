7d72fc92908e00f15f0f643f433decf0338da0a4
Dawei Huang <dv888000@gmail.com.tw>
2020-05-08 09:50:35 -0400

    number provided by others

--- guessnumber.py
+++ guessnumber.py
@@ -1,5 +1,10 @@
 import random
-r = random.randint(1,100)
+start = input('請輸入數字起始值:')
+end = input('請輸入數字終止值:')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
@@ -7,11 +12,11 @@ while True:
 	num = int(num)
 	if num == r:
 		print('你猜對囉')
-		print('你已經猜了', count, '次')
+		print('一共猜了', count, '次')
 		break
 	elif num > r:
 		print('比答案大,再猜看看')
 	elif num < r:
 		print('比答案小,再猜看看')
-	print('你已經猜了', count, '次')
+	print('一共猜了', count, '次')
 		
